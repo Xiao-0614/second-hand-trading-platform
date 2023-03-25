f7920fdf6df2fb38f5b41a03e06ee9abb8317b64
Xiao Zhang <chloez0614@gmail.com>
2023-03-25 15:03:28 -0500

    initial commit

---
+++ src/test/java/com/chloekarah/secondhandplatform/SecondHandPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chloekarah.secondhandplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondHandPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
