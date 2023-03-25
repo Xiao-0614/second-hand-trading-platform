5b577bca441ca26b31b3a949b53c2278087b4cd0
Xiao Zhang <chloez0614@gmail.com>
2023-03-25 15:43:13 -0500

    add dependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,11 @@
 rootProject.name = "second-hand-platform"
+
+include ("user-service")
+include ("upload-service")
+include ("post-service")
+include ("monitoring-service")
+include ("matching-service")
+include ("feed-generation-service")
+include("chat-service")
+include("cache-service")
+
